24e0d259efe36d4bebf4c61e801a4a745b07f108
KopandaXp <kopanda@mail.ru>
2017-09-24 18:04:07 +0300

    1
    1

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,202 +1,64 @@
-var oParam = {};
-var oHeader = {};
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/odata/v2/ODataModel",
+	"sapTwitter/util/Formatter",
+	"sap/m/MessageBox"
+],
+function(
+	UIComponent,
+	JSONModel,
+	ODataModel,
+	Formatter,
+	MessageBox
+) {
+	"use strict";
 
-function to_rfc3986(s) {
-	var tmp = encodeURIComponent(s);
-	tmp = tmp.replace(/!/g, '%21');
-	tmp = tmp.replace(/\*/g, '%2A');
-	tmp = tmp.replace(/\(/g, '%28');
-	tmp = tmp.replace(/\)/g, '%29');
-	tmp = tmp.replace(/\'/g, '%27');
-	return tmp;
-}
+	return UIComponent.extend("sapTwitter.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function () {
+			// Отключаем кэш, чтобы не было ошибки, связанной с отсутствием файла Component-changes.json
+			sap.ui.fl.Cache.setActive(false);
+			// this.applyPatch();
+			// Объявляем одата модели
+			var oComponent = this;
+			var oResourceBundle = oComponent.getModel("i18n").getResourceBundle();
+			window.goController = {};
+			document.title = oResourceBundle.getText("appTitle");
+			var oComponent = window.goComponent = this;
+			var oGlobalModel = window.goGlobalModel = new JSONModel({
 
-// accepts object with additional parameters.
-// sURLPrefix 
-// function makeAuthorizationString (sURLPrefix, sURLParameters, Parameters) {
-//     var oAPIData = {
-// 		status: "Hello World"
-// 	};
-// 	var sMethod = "POST",
-// 		sOrigin = oHeader.clientprotocol + "://" + oHeader["~server_name"],
-// 		sBaseURL = sOrigin + sURLPrefix, // "/twitter/1.1/statuses/update.json",
-// 		// sURLParameters = "?include_entities=true",
-// 		sURL = sBaseURL + sURLParameters,
-// 		// sStatus = "Hello world",
-// 		// sIncludeEntities = "true",
-// 		sOAuthConsumerKey = "mjpp4cUeysMM0ruMjjPCjJMZc",
-// 		sOAuthNonce = btoa(Math.random() + "" + Math.random() + Math.random() + Math.random()).replace(/[^a-zA-Z0-9]/g, ""),
-// 		sOAuthSignatureMethod = "HMAC-SHA1",
-// 		sOAuthTimestamp = Math.floor(Date.now() / 1000) + "",
-// 		sOAuthToken = "",
-// 		sOAuthVersion = "1.0";
-// 		// oauth_callback="http%3A%2F%2Flocalhost%2Fsign-in-with-twitter%2F",
-// 		// oauth_consumer_key="cChZNFj6T5R0TigYB9yd1w",
-// 		// oauth_nonce="ea9ec8429b68d6b77cd5600adbbb0456",
-// 		// oauth_signature="F1Li3tvehgcraF8DMJ7OyxO4w9Y%3D",
-// 		// oauth_signature_method="HMAC-SHA1",
-// 		// oauth_timestamp="1318467427",
-// 		// oauth_version="1.0"
-// 	var oOAuthKeys = {
-// 		status: sStatus,
-// 		include_entities: sIncludeEntities,
-// 		oauth_consumer_key: sOAuthConsumerKey,
-// 		oauth_nonce: sOAuthNonce,
-// 		oauth_signature_method: sOAuthSignatureMethod,
-// 		oauth_timestamp: sOAuthTimestamp,
-// 		oauth_token: sOAuthToken,
-// 		oauth_version: sOAuthVersion
-// 	};
-// 	var aOAuthKeys = [];
-// 	for (var i in oOAuthKeys) {
-// 		aOAuthKeys.push(to_rfc3986(i) + "=" + to_rfc3986(oOAuthKeys[i]));
-// 	}
-// 	aOAuthKeys.sort(function (a, b) {return a > b;});
-// 	var sOAuthKeys = aOAuthKeys.join("&");
-// 	var sOAuthSignatureBaseString =
-// 		sMethod + "&" +
-// 		to_rfc3986(sBaseURL) + "&" +
-// 		to_rfc3986(sOAuthKeys);
-	
-// }
-
-function reveal (oTarget) {
-	var oRevealed = {};
-	for (var i in oTarget) {
-		// if (oTarget.hasOwnProperty(i) ||
-		// 	oTarget.__proto__.hasOwnProperty(i)) {
-			try {
-				oRevealed[i] = oTarget[i];
-				
-			} catch (e) {
-				oRevealed[i] = e.message;
-			}
-		// }
-	}
-	return {result: oRevealed, target: oTarget};
-}
-//Implementation of GET call
-function handleGet() {
-	// Retrieve data here and return results in JSON/other format 
-	$.response.status = $.net.http.OK;
-	return {"myResult": "GET success"};
-}
-//Implementation of POST call
-function handlePost() {
-	var bodyStr = $.request.body ? $.request.body.asString() : undefined;
-	if (bodyStr === undefined) {
-		$.response.status = $.net.http.INTERNAL_SERVER_ERROR;
-		return {"myResult": "Missing BODY"};
-	}
-	// Extract body insert data to DB and return results in JSON/other format
-	$.response.status = $.net.http.CREATED;
-	return {"myResult": "POST success"};
-}
-// Check Content type headers and parameters
-function validateInput() {
-	var i, j;
-	var paramName, paramValue;
-	var headerName, headerValue;
-	// var contentType;
-
-	// Check content-type is application/json
-	// contentType = $.request.contentType;
-	// if (contentType === null || contentType.startsWith("application/json") === false) {
-	// 	 $.response.status = $.net.http.INTERNAL_SERVER_ERROR;
-	// 	 $.response.setBody("Wrong content type request use application/json");
-	// 	return false;
-	// }
-	// Extract parameters and process them 
-	for (i = 0; i < $.request.parameters.length; ++i) {
-		paramName = $.request.parameters[i].name;
-		paramValue = $.request.parameters[i].value;
-		oParam[paramName] = paramValue;
-	}
-	// Extract headers and process them 
-	for (j = 0; j < $.request.headers.length; ++j) {
-		headerName = $.request.headers[j].name;
-		headerValue = $.request.headers[j].value;
-		oHeader[headerName] = headerValue;
-	}
-	return true;
-}
-// Request process 
-function processRequest() {
-	if (validateInput()) {
-		try {
-			switch ($.request.method) {
-				//Handle your GET calls here
-				case $.net.http.GET:
-				// 	$.response.setBody(JSON.stringify(handleGet()));
-					
-				break;
-
-				//Handle your POST calls here
-				case $.net.http.POST:
-				// 	$.response.setBody(JSON.stringify(handlePost()));
-					
-				break;
-
-				//Handle your other methods: PUT, DELETE
-				default:
-					$.response.status = $.net.http.METHOD_NOT_ALLOWED;
-				// 	$.response.setBody("Wrong request method");
-					throw new Error("Wrong request method");
-			}
-			$.response.contentType = "application/json";
+			});
+			oComponent.setModel(oGlobalModel, "globalModel");
 			
-			var oAPIData = {
-				status: "Hello World"
-			};
-			var sMethod = "POST",
-				sOrigin = oHeader.clientprotocol + "://" + oHeader["~server_name"],
-				sBaseURL = sOrigin + "/twitter/1.1/statuses/update.json",
-				sURLParameters = "?include_entities=true",
-				sURL = sBaseURL + sURLParameters,
-				sStatus = "Hello world",
-				sIncludeEntities = "true",
-				sOAuthConsumerKey = "mjpp4cUeysMM0ruMjjPCjJMZc",
-				sOAuthNonce = btoa(Math.random() + "" + Math.random() + Math.random() + Math.random()).replace(/[^a-zA-Z0-9]/g, ""),
-				sOAuthSignatureMethod = "HMAC-SHA1",
-				sOAuthTimestamp = Math.floor(Date.now() / 1000) + "",
-				sOAuthToken = "",
-				sOAuthVersion = "1.0";
-				
-			var oOAuthKeys = {
-				status: sStatus,
-				include_entities: sIncludeEntities,
-				oauth_consumer_key: sOAuthConsumerKey,
-				oauth_nonce: sOAuthNonce,
-				oauth_signature_method: sOAuthSignatureMethod,
-				oauth_timestamp: sOAuthTimestamp,
-				oauth_token: sOAuthToken,
-				oauth_version: sOAuthVersion
-			};
-			var aOAuthKeys = [];
-			for (var i in oOAuthKeys) {
-				aOAuthKeys.push(to_rfc3986(i) + "=" + to_rfc3986(oOAuthKeys[i]));
-			}
-			aOAuthKeys.sort(function (a, b) {return a > b;});
-			var sOAuthKeys = aOAuthKeys.join("&");
-			var sOAuthSignatureBaseString =
-				sMethod + "&" +
-				to_rfc3986(sBaseURL) + "&" +
-				to_rfc3986(sOAuthKeys);
-			
-			var dest = $.net.http.readDestination("TwitterXS.webapp.xsjs", "dst_twitter_api");
-			var client = new $.net.http.Client();
-			client.setTimeout(5); // timeout in seconds;
-			var req = new $.web.WebRequest($.net.http.POST, sURLParameters);
-// 			var req = new $.net.http.Request($.net.http.POST, JSON.stringify(oAPIData));
-			req.setBody(JSON.stringify(oAPIData));
-			client.request(req, dest);
-
-			var response = client.getResponse();
-			$.response.setBody("Status: " + response.status);
-		} catch (e) {
-			$.response.setBody("Failed to execute action: " + typeof e === "string" ? e : e.toString());
+			UIComponent.prototype.init.apply(this, arguments);
+			oComponent.getRouter().initialize();
 		}
-	}
-}
-// Call request processing
-processRequest();
+		// ,
+		// applyPatch: function () {
+		// 	String.prototype.to_rfc3986 = function () {
+		// 		var tmp = encodeURIComponent( this );
+		// 		tmp = tmp.replace( /!/g,'%21' );
+		// 		tmp = tmp.replace( /\*/g,'%2A' );
+		// 		tmp = tmp.replace( /\(/g,'%28' );
+		// 		tmp = tmp.replace( /\)/g,'%29' );
+		// 		tmp = tmp.replace( /\'/g,'%27' );
+		// 		return tmp;
+		// 	};
+		// }
+	});
+});
+
+// window.convertCMYKtoRGB = function (sCMYK) {
+// 	// принимает строку вида 11 22 33 44
+// 	var aCMYK = sCMYK.split(" ");
+// 	var c = aCMYK[0], m = aCMYK[1], y = aCMYK[2], k = aCMYK[3];
+// 	c /= 100; m /= 100; y /= 100; k /= 100;
+// 	var r, g, b;
+// 	r = Math.min(255, Math.ceil(255 * (1 - c) * (1 - k)));
+// 	g = Math.min(255, Math.ceil(255 * (1 - m) * (1 - k)));
+// 	b = Math.min(255, Math.ceil(255 * (1 - y) * (1 - k)));
+// 	return "rgb(" + r + ", " + g + ", " + b + ");";
+// }
